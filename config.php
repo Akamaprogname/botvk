71f188bb9a79e88cf18ce64dba5392242845f529
SERGAY <mathinov@gmail.com>
2020-02-23 02:15:50 +0600

    pp

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BOT_BASE_DIRECTORY', '/var/www');
-define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
-define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
-define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
-
-define('CALLBACK_API_CONFIRMATION_TOKEN', getenv('CALLBACK_API_CONFIRMATION_TOKEN')); //Строка для подтверждения адреса сервера из настроек Callback API
-define('VK_API_ACCESS_TOKEN', getenv('VK_API_ACCESS_TOKEN')); //Ключ доступа сообщества
---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+import vk_api
+import os
+
+app = Flask(__name__)
+
+vk = vk_api.VkApi(token=os.environ['VK_API_ACCESS_TOKEN'])
+vk.auth()
+
+@app.route("/", methods=['POST', 'GET'])
+def hello():
+    if request.method == 'GET':
+        # Default hujeta
+        return "This is bot!"
+    else:
+        # Real logic
+        requestJson = request.get_json(force=True)
+
+        if requestJson['type'] == 'message_new':
+            userId = request['object']['from_id']
+            vk.method('message.send', {'user_id': userId, 'message': 'Pashol nahui'})
+        else:
+            return "Unsupported request type"
+
+        return "Some logic ebat'"
